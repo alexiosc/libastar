415115b6e6ac2b4b5fec718b212f527c84abc9e0
Alexios Chouchoulas <alexios@bedroomlan.org>
2009-02-14 08:05:04 +0000

    Added boilerplat debian files.

---
+++ debian/libastar.default.ex
@@ -0,0 +1,10 @@
+# Defaults for libastar initscript
+# sourced by /etc/init.d/libastar
+# installed at /etc/default/libastar by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibastar 0.9 libastar (>> 0.9-0), libastar (<< 0.9-99)
