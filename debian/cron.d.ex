7c421dd34c803c2af7e91970dc62640c9d0fb17c
Alexios Chouchoulas <alexios@bedroomlan.org>
2009-02-14 08:27:00 +0000

    Initial set of modifications to debian build infrastructure.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libastar package
-#
-0 4	* * *	root	[ -x /usr/bin/libastar_maintenance ] && /usr/bin/libastar_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libastar):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libastar" command="/usr/bin/libastar"
