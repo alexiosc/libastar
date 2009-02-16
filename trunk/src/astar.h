7895ad57ddd7d6a339955d86b609df7bab534017
alexios <alexios@d1817a3e-e191-49ac-a0dc-a8e36f4b10f9>
2009-02-16 12:03:48 +0000

    Changes to accommodate documentation generation.

--- trunk/src/astar.h
+++ trunk/src/astar.h
@@ -77,6 +77,10 @@ typedef uint8_t direction_t;
  * this case internally. The caller should have massaged numbers
  * appropriately.
  *
+ * If your map is sufficiently small, you can find paths on the entire map by
+ * setting the origin to (0,0) and making the width and height match those of
+ * your entire map.
+ *
  */
 
 typedef struct {
@@ -227,12 +231,12 @@ typedef struct {
 
 // We use three bits to specify the direction of a square's 'parent'.
 #define DIR_N  0
-#define DIR_E  1
-#define DIR_S  2
-#define DIR_W  3
-#define DIR_NE 4
-#define DIR_SE 5
-#define DIR_SW 6
+#define DIR_E  2
+#define DIR_S  4
+#define DIR_W  6
+#define DIR_NE 1
+#define DIR_SE 3
+#define DIR_SW 5
 #define DIR_NW 7
 
 // Movement modes.
@@ -244,22 +248,162 @@ typedef struct {
 // added safety).
 #define DIR_END 255
 
-
+/** 
+ * Initialise A*.
+ * 
+ * @param w The width of the search space in grid squares.
+ *
+ * @param h The height of the search space in grid squares.
+ *
+ * @param get A map cost getter. This must accept a set of <tt>uint32_t</tt>
+ *        co-ordinates in X,Y order and must return a uint8_t number to signify
+ *        the cost of stepping onto the specified square. Values for passable
+ *        (non-blocked) squares range from 0 to 254. The special value
+ *        <tt>COST_BLOCKED</tt> means the grid square is impassable (a wall).
+ *
+ * @param heuristic A heuristic function with the following signature:
+ *        <code>uint32_t heuristic (uint32_t x0, uint32_t y0, uint32_t x1,
+ *        uint32_t y1)</code>.
+ *        It requires two sets of (X,Y) coordinates and
+ *        returns a <tt>uint32_t</tt> number that corresponds to the estimated
+ *        distance between the two co-ordinates, or estimated cost to travel
+ *        from the first to the second. You may pass <tt>NULL</tt>, in which
+ *        case the built-in Manhattan Distance heuristic is used. This
+ *        parameter may be NULL, in which case the built-in
+ *        manhattan_distance() heuristic is used instead.
+ * 
+ * @return A pointer to a new astar_t structure, an A* algorithm handle.
+ */
 astar_t *
 astar_new (const uint32_t w, const uint32_t h,
 	   uint8_t (*get) (const uint32_t, const uint32_t),
 	   uint32_t  (*heuristic) (const uint32_t, const uint32_t,
 				   const uint32_t, const uint32_t));
+
+
+/** 
+ * Initialise fully the A* grid.
+ *
+ * To interface your map with the (possibly smaller) map kept in the
+ * <tt>astar_t</tt> structure, you can either let A* obtain only the map
+ * squares it needs when it needs them, or you can fully initialise the map
+ * beforehand (this also sets the origin and the map getter
+ * callback). Depending on the structure of your program and how many times you
+ * run astar_run() on the same <tt>astar_t</tt> structure, this may be a faster
+ * option than obtaining map squares one by one, on demand. Profiling your code
+ * may help decide which is the preferable method.
+ *
+ * @param as An initialised A* context.
+ * @param origin_x The X origin (leftmost row) of the A* map on the game map.
+ * @param origin_y The Y origin (topmost row) of the A* map on the game map.
+ * @param get A map cost getter. This must accept a set of <tt>uint32_t</tt>
+ *        co-ordinates in X,Y order and must return a uint8_t number to signify
+ *        the cost of stepping onto the specified square. Values for passable
+ *        (non-blocked) squares range from 0 to 254. The special value
+ *        <tt>COST_BLOCKED</tt> means the grid square is impassable (a wall).
+ */
 	   
 void astar_init_grid (astar_t * as,
 		      uint32_t origin_x, uint32_t origin_y,
 		      uint8_t(*get)(const uint32_t, const uint32_t));
 
+/** 
+ * Set cardinal or eight-way pathfinding mode.
+ *
+ * Setting this will affect any pending and subsequent invocations of
+ * astar_run().
+ * 
+ * @param as An initialised A* context.
+ *
+ * @param movement_mode either <tt>DIR_CARDINAL</tt> (search for paths using
+ * only the four cardinal directions) or <tt>DIR_8WAY</tt> (search for paths
+ * using all eight directions).
+ */
+
 void astar_set_movement_mode (astar_t * as, int movement_mode);
+
+/** 
+ * Retrieve the path found my A*
+ *
+ * This function returns (by reference) an array of direction_t elements. Each
+ * direction_t element represents one step from the starting location towards
+ * the target location. For full paths, the directions eventually reach this
+ * target location. Partial paths reach a compromise point as near the target
+ * location as could be found by A*.
+ *
+ * A direction_t element has one of eight values (if the pathfinding mode is
+ * <tt>DIR_CARDINAL</tt>, only the four first ones are used).
+ *   - <tt>DIR_N</tt>
+ *   - <tt>DIR_E</tt>
+ *   - <tt>DIR_S</tt>
+ *   - <tt>DIR_W</tt>
+ *   - <tt>DIR_NE</tt>
+ *   - <tt>DIR_SE</tt>
+ *   - <tt>DIR_SW</tt>
+ *   - <tt>DIR_NW</tt>
+ *
+ * The list of directions is terminated with a <tt>DIR_END</tt> value, much
+ * like C strings are terminated with a '\0' char value. Additionally, the
+ * number of steps in the directions (excluding the <tt>DIR_END</tt> marker) is
+ * also returned by this function.
+ *
+ * Each represents a step in the corresponding direction. These may be
+ * translated into (dx,dy) steps (e.g. <tt>DIR_NE</tt> will translate to a
+ * (dx,dy) pair of (-1,1) using the default configuration) by calling
+ * astar_get_dx() and astar_get_dy() respectively.
+ * 
+ * The directions may be used to move a unit step by step along the found path,
+ * or they may be looped through to perform the move in one. The example code
+ * (example.c) uses such a loop through the directions to annotate the found
+ * path on a map.
+ * 
+ * @warning The function astar_free_directions() must be used with
+ * <tt>directions</tt> as its argument to deallocate the directions array when
+ * you are done processing it. If this is not done, a memory leak will occur.
+ *
+ * @param as An initialised A* context.
+ *
+ * @param directions An unallocated variable of type <tt>direction_t *</tt>
+ * passed by reference. This will be set to point to a newly allocated array of
+ * direction_t elements that signify steps from the starting point of the path
+ * to the destination (for full paths) or best compromise (for partial paths).
+ * 
+ * @return The number of steps returned.
+ *
+ */
 
 uint32_t astar_get_directions (astar_t *as, direction_t ** directions);
 
+/** 
+ * Free memory used by a path.
+ *
+ * Call this function to deallocate the directions_t array yielded by
+ * astar_get_directions(). Failure to do so will cause memory to leak.
+ * 
+ * @param directions An array of direction_t elements previously allocated by
+ * astar_get_directions().
+ */
+
 void astar_free_directions (direction_t * directions);
+
+/** 
+ * Set the origin of the path finding map.
+ *
+ * A* may be used to locate a path in an area smaller than the entire game
+ * map. To do so, the origin (top-left co-ordinates) of the A* map on the game
+ * map must be set using this function. also, the width and height parameters
+ * passed to astar_new() must be set to values smaller than the full game
+ * map's.
+ *
+ * This function may be used to move the origin around. This is a useful
+ * technique in conserving CPU cycles if a game unit is meant to plan in small
+ * chunks. Using it, you may form small path finding plans (which are not
+ * guaranteed to be optimal) and move a unit incrementally.
+ * 
+ * @param as An initialised A* context.
+ * @param x The X co-ordinate of the origin.
+ * @param y The Y co-ordinate of the origin.
+ */
 
 void astar_set_origin (astar_t * as, const uint32_t x, const uint32_t y);
 
@@ -275,6 +419,42 @@ void astar_set_steering_penalty (astar_t *as, const uint32_t steering_penalty);
 
 void astar_set_heuristic_factor (astar_t *as, const uint32_t heuristic_factor);
 
+/** 
+ * Run the A* algorithm.
+ *
+ * The algorithm will run until the target location is reached, or until one of
+ * the running constraints (cost, time, etc) are reached.
+ * 
+ * @param as An initialised A* context.
+ * @param x0 The X ordinate of the starting location.
+ * @param y0 The Y ordinate of the starting location.
+ * @param x1 The X ordinate of the target location.
+ * @param y1 The Y ordinate of the target location.
+ * 
+ * @return A <tt>ASTAR_</tt>x result code.
+ *
+ *   - <tt>ASTAR_FOUND</tt> if returned if a full route was found. The function
+ *        astar_get_directions() may be used to retrieve the full path.
+ *   - <tt>ASTAR_NOTFOUND</tt> means the target location could not be
+ *        reached. A partial path to a location as near the target as possible
+ *        may be available. Use astar_have_route() to check if one is
+ *        available.
+ *   - <tt>ASTAR_TRIVIAL</tt> denotes the starting location is the same as the
+ *        target location. No work was done.
+ *   - <tt>ASTAR_EMBEDDED</tt> denotes the starting location is on a 'wall'
+ *        square (a square for which the cost is <tt>COST_BLOCKED</tt>).
+ *        No work was done.
+ *   - <tt>ASTAR_TIMEOUT</tt> is returned when a timeout was set and the
+ *        allotted time ran out before a (full) route was found. A partial path
+ *        to a location as near the target as possible may be available. Use
+ *        astar_have_route() to check.
+ *   - <tt>ASTAR_GRID_NOT_INITIALISED</tt> (or <tt>ASTAR_GRID_NOT_INITIALIZED</tt>)
+ *        is an error condition thrown when a map getter function hasn't been
+ *        set and astar_init_grid() hasn't been called in this astar_t context.
+ *   - <tt>ASTAR_ORIGIN_NOT_SET</tt> is an error condition thrown when the map
+ *        origin hasn't been set yet.
+ *
+ */
 int astar_run (astar_t * as,
 	       const uint32_t x0, const uint32_t y0,
 	       const uint32_t x1, const uint32_t y1);
